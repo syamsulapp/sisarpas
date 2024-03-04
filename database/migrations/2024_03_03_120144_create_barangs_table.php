d61fa28e93d13440e4b9d041770cd4a29a40ea3c
syamsulapp <samsul.marif100@gmail.com>
2024-03-04 23:24:20 +0800

    update permission

---
+++ database/migrations/2024_03_03_120144_create_barangs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->string('id_barang')->unique()->primary();
+            $table->string('name');
+            $table->string('condition');
+            $table->enum('status', ['ready', 'not-ready']);
+            $table->string('specification');
+            $table->string('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
