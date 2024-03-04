896a18b9f205311272f388da10844bde7fe8b0c3
syamsulapp <samsul.marif100@gmail.com>
2024-03-04 23:50:13 +0800

    init migration db

--- database/migrations/2024_03_03_120144_create_barangs_table.php
+++ database/migrations/2024_03_03_120144_create_barangs_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('barangs', function (Blueprint $table) {
             $table->string('id_barang')->unique()->primary();
-            $table->string('name');
-            $table->string('condition');
-            $table->enum('status', ['ready', 'not-ready']);
-            $table->string('specification');
-            $table->string('stock');
+            $table->string('name_barang');
+            $table->string('condition_barang');
+            $table->enum('status_barang', ['ready', 'not-ready']);
+            $table->string('specification_barang');
+            $table->string('stock_barang');
+            $table->string('img_barang');
             $table->timestamps();
         });
     }
