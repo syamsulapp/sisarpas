5588b843b2c9df58b535ba034313a146d45e63c5
syamsulapp <samsul.marif100@gmail.com>
2024-03-06 22:29:41 +0800

    implement dashboard themes

--- resources/views/sisarpas/user/dashboard/index.blade.php
+++ resources/views/sisarpas/user/dashboard/index.blade.php
@@ -1,15 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('sisarpas.user.layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('title', 'Dashboard User');
 
-<body>
-    <p>halo dashboard user ini</p>
-</body>
+@section('content-user-dashboard')
+    <!-- Content wrapper -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-lg-12 mb-4 order-0">
+                <div class="card">
+                    <div class="d-flex align-items-end row">
+                        <div class="col-sm-7">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Selamat Datang Miftahul Jannah! 🎉</h5>
+                                <p class="mb-4">Anda bisa melihat data peminjaman barang ataupun data pengembalian barang
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-5 text-center text-sm-left">
+                            <div class="card-body pb-0 px-0 px-md-4">
+                                <img src="{{ asset('sisarpas/assets/admin/assets/img/illustrations/Online test-pana.png') }}"
+                                    height="140" alt="View Badge User"
+                                    data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                                    data-app-light-img="illustrations/man-with-laptop-light.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / Content -->
 
-</html>
+@endsection
---
+++ resources/views/sisarpas/user/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+ <!-- Footer -->
+ <footer class="content-footer footer bg-footer-theme">
+     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+         <div class="mb-2 mb-md-0">
+             &copy;
+             <script>
+                 document.write(new Date().getFullYear());
+             </script>
+             Copyright <strong><span>ITERA</span></strong>. Institut Teknologi Sumatera
+         </div>
+     </div>
+ </footer>
+ <!-- / Footer -->
