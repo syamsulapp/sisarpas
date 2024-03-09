6c78ae9ac00de5c1857de018308f846e1e47d625
syamsulapp <samsul.marif100@gmail.com>
2024-03-09 14:31:45 +0800

    create migration for table landing and contacts

---
+++ database/migrations/2024_03_09_130124_create_landings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('landings', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->string('video')->nullable();
+            $table->enum('type', ['image', 'video']);
+            $table->enum('status', ['hide', 'unhide']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('landings');
+    }
+};
