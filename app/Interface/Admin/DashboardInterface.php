4dd49b9285e60dce9c0ee97b03a6f786b51a1889
syamsulapp <samsul.marif100@gmail.com>
2024-03-10 17:08:38 +0800

    fix update landing

--- app/Interface/Admin/DashboardInterface.php
+++ app/Interface/Admin/DashboardInterface.php
@@ -5,6 +5,6 @@ namespace App\Interface\Admin;
 interface DashboardInterface
 {
     public function createLandingRepositories($request): void;
-    public function deleteLandingRepositories($request): void;
-    public function updateLandingRepositories($id): void;
+    public function deleteLandingRepositories($id): void;
+    public function updateLandingRepositories($request): void;
 }
