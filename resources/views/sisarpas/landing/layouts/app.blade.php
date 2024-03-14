418136936a63ca2c4ce17b001713f717432507ac
syamsulapp <samsul.marif100@gmail.com>
2024-03-14 22:59:38 +0800

    fix request app.blade.php for header

--- resources/views/sisarpas/landing/layouts/app.blade.php
+++ resources/views/sisarpas/landing/layouts/app.blade.php
@@ -32,7 +32,9 @@
 <body>
 
     <!-- header with condition -->
-    @if (Request::is('peminjaman/alat/barang') || Request::is('peminjaman/ruangan/aula'))
+    @if (Request::is('peminjaman/alat/barang') ||
+            Request::is('peminjaman/ruangan/aula') ||
+            Request::is('peminjaman/cari/barang'))
         <!-- header peminjaman -->
         @include('sisarpas.landing.layouts.peminjaman.header')
     @else
