d8f678a64ae9a0d16e5bc94a0dc172dd2a7e3c80
syamsulapp <samsul.marif100@gmail.com>
2024-03-17 10:32:44 +0800

    migration for schema tbl jadwal ruangan(aula)

---
+++ database/migrations/2024_03_17_092636_create_schedule_room_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedule_room', function (Blueprint $table) {
+            $table->id();
+            $table->string('barangs_id')->references('id')->on('barangs');
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedule_room');
+    }
+};
