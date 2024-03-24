86272bd4fdfe01eaad2290404e44b2c5b3417667
syamsulapp <samsul.marif100@gmail.com>
2024-03-24 10:13:23 +0800

    create migration for save informasi penting

---
+++ database/migrations/2024_03_24_090745_create_informasi_pentings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('informasi_pentings', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul_informasi');
+            $table->text('isi_informasi');
+            $table->string('gambar_informasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('informasi_pentings');
+    }
+};
